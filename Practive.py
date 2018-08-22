74a936bbacd78cd25d010d22f6ad063ff76e2730
Etsuya Hitori <42312804+Etsuya233@users.noreply.github.com>
2018-08-22 10:58:57 +0800

    Practice.py
    Nanimonai!

---
+++ Practive.py
@@ -0,0 +1,49 @@
+print('欢迎来到MekOs')
+print('请输入 v 来了解此软件，输入 o 为已阅。')
+reader = input('请输入: ')
+if reader == 'v':
+    print('Yes')
+elif reader == 'o':
+    print('Angry！')
+else:
+    print('请输入正确的字符!')
+    qwq = True
+    while qwq:
+        xyb = input('Please: ')
+        if xyb == 'v':
+            print('Yes')
+            qwq = False
+        elif xyb == 'o':
+            print('Angry!')
+            qwq = False
+        else:
+            print('请输入正确的字符！')
+
+un = input('请输入用户名: ')
+if un == 'Etsuya':
+    print('Yes')
+elif un != 'Etsuya':
+    qwqwq = True
+    while qwqwq:
+        un = input('请输入用户名: ')
+        if un == 'Etsuya':
+            qwqwq = False
+        elif un != 'Etsuya':
+            print('请重新输入用户名！')
+
+pw = input('请输入密码: ')
+if pw == "fnndp":
+    print('Yes')
+elif pw != "fnndp":
+    qqwq = True
+    while qqwq:
+        pw = input('请输入密码: ')
+        if pw == 'fnndp':
+            qqwq = False
+        elif pw != 'fnndp':
+            print('请重新输入用户名!')
+            
+
+
+print('已完成！')
+print('Etsuya!')
